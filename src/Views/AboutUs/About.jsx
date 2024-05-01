cb094614bdc14669629efa4b772adb093503cdbb
Maxip32 <piolattoemanuel@gmail.com>
2024-05-01 19:43:00 -0300

    actualizacion 1.5.2024

--- src/Views/AboutUs/About.jsx
+++ src/Views/AboutUs/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
               src="https://res.cloudinary.com/dyeknjnsi/image/upload/v1692923479/Rincon%20de%20luz/WhatsApp_Image_2023-08-24_at_21.30.19_seliil.jpg"
             />
             <div className="flex-1  my-4">
-              <p className="text-xl font-semibold leadi">Silvia Gandino</p>
+              <p className="text-xl font-semibold leadi">+543584448512</p>
               <p>Contacto</p>
             </div>
             <div className="flex items-center justify-center p-3 space-x-3 border-t-2">
@@ -24,21 +24,21 @@ const About = () => {
             
               <a
                 rel="noopener noreferrer"
-                href="https://www.instagram.com/santeria_rincondeluz/?igshid=MzRlODBiNWFlZA%3D%3D"
+                href="https://www.instagram.com/libreria_regaleriamp/?igshid=MzRlODBiNWFlZA%3D%3D"
                 title="Instagram"
                 className="dark:text-gray-900 hover:dark:text-violet-400"
               >
                 
                 <img src="https://w7.pngwing.com/pngs/477/609/png-transparent-logo-computer-icons-instagram-logo-instagram-logo-miscellaneous-text-trademark.png" alt="Logo" className="w-6 h-6" />
               </a>
-              <a
+              {/* <a
                 rel="noopener noreferrer"
                 href="https://www.facebook.com/profile.php?id=100090125044453&mibextid=9R9pXO"
                 title="Facebook"
                 className="dark:text-gray-900 hover:dark:text-violet-400"
               >
                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/2048px-Facebook_f_logo_%282019%29.svg.png" alt="Logo" className="w-6 h-6" />
-              </a>
+              </a> */}
              
                 
              
